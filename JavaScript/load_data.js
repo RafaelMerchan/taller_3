4aaf46fe7262b7dc122f7c20ab17e34b4530e0e9
RafaelMerchan <73255395+RafaelMerchan@users.noreply.github.com>
2023-11-15 10:55:28 -0500

    Update load_data.js

--- JavaScript/load_data.js
+++ JavaScript/load_data.js
@@ -19,14 +19,14 @@ let buttonListener = async (event) => {
              <div class="col">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title"><a href=" -phonetics-sourceUrl- " target="_blank"> responseJSON.word </a></h5>
+                        <h5 class="card-title"><a href=" -phonetics-sourceUrl- " target="_blank"> ${responseJSON.word} </a></h5>
                         <audio controls>
-                        <source src=" responseJSON.phonetics.audio " type="audio/mpeg">
+                        <source src=" ${responseJSON.phonetics.audio} " type="audio/mpeg">
                         Your browser does not support the audio element.
                         </audio>
-                        <h6 class="card-subtitle mb-2 text-muted"> responseJSON.phonetics.text </h6>
-                        <p class="card-text text-primary"> responseJSON.meanings.synonyms </p>
-                        <p class="card-text text-danger"> responseJSON.meanings.antonyms </p>
+                        <h6 class="card-subtitle mb-2 text-muted"> ${responseJSON.phonetics.text} </h6>
+                        <p class="card-text text-primary"> ${responseJSON.meanings.synonyms} </p>
+                        <p class="card-text text-danger"> ${responseJSON.meanings.antonyms} </p>
                     </div>
                 </div>
             </div>
